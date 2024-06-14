da866157b5673ecb49d8384c0f644e36f880d678
IrynaMaroz <maroz.iryna@gmail.com>
2024-06-14 12:32:13 +0300

    added one more example

--- Program.cs
+++ Program.cs
@@ -2,10 +2,48 @@
 
 Console.WriteLine(value: "Start of the App");
 
-var people = DataAccessHelpers.GetPeople().Take(count: 2);
+//var people = DataAccessHelpers.GetPeople().Take(count: 2);
 
-foreach (var p in people)
+//foreach (var p in people)
+//{
+//    Console.WriteLine(value: $"Read {p.FirstName} {p.LastName}");
+//}
+
+var primeNumbers = Generators.GetPrimeNumbers().Take(10);
+foreach (var prime in primeNumbers)
 {
-    Console.WriteLine(value: $"Read {p.FirstName} {p.LastName}");
+    Console.WriteLine(prime);
 }
+
 Console.WriteLine(value: "End of the App");
+
+public class Generators
+{
+    public static IEnumerable<int> GetPrimeNumbers()
+    {
+        int counter = 1;
+        while(true)
+        {
+            if(IsPrimeNumber(counter))
+            {
+                yield return counter; 
+            }
+
+            counter++;
+        }
+    }
+
+    private static bool IsPrimeNumber (int value)
+    {
+        bool output = true;
+        for (int i = 2; i < value / 2; i++)
+        {
+            if (value % i == 0)
+            {
+                output = false; 
+                break;
+            }
+        }
+        return output;
+    }
+}
